21b66cf0f7b156270a8ebb3fd57a05b41390e94a
jreich08 <reichelj@hartwick.edu>
2025-02-05 08:50:59 -0500

    Test Writing and Saving File

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,12 @@
-print("John Reichel wrote this!!")
+print("John Reichel wrote this!!\nThis is a continuation of line one!?\nHow cool is that all on one!")
+print("How's it going?")
+print("I am doing fantastic!")
+print("""Hello
+This is the triple quote attempt 
+This is a bit easier than the slash and the "N" overall.
+Pretty cool!""")
+print("Hello world! Can you say \"Hi\"?")
+print("Please save your work to your C:\\Drive")
+print("Here is a tab:\t string")
+print("Here is a double tab:\t\tstring")
+print("""If you are using the "\\t" command make sure you get rid of any leading spaces.""")
