c9178bba0ab8daab12e642595016b472a1b9bdc1
jreich08 <reichelj@hartwick.edu>
2025-02-11 14:55:42 -0500

    Work on this. Make an advanced tree and circle it with red. Then go back into the code and create logical breaks with notes using the # sign.

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -2,21 +2,29 @@ import arcade
 # import the "arcade" library
 arcade.open_window(width=600, height=600, window_title="Drawing Test")
 # This defines the size of the drawing window
+
+#Set Background
 arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 arcade.start_render()
 
 arcade.draw_lrtb_rectangle_filled(0,600,300,0,arcade.csscolor.DARK_GREEN)
 # ready to draw starts drawing
 # Draw a rectangle
-# Left of 0, right of 599
+# Left of 0, right of 600
 # Top of 300, bottom of 0
+
+
+#Circle Based Tree
 arcade.draw_xywh_rectangle_filled(100,300,20,60,arcade.csscolor.SIENNA)
 arcade.draw_circle_filled(110,350,30, arcade.csscolor.LIGHT_GREEN)
+#Elipse Based Trees
 arcade.draw_xywh_rectangle_filled(180,300,20, 60, arcade.csscolor.SIENNA)
 arcade.draw_ellipse_filled(190, 370,60, 80, arcade.csscolor.LIGHT_GREEN )
+#Failed Pine Tree with Arcs
 arcade.draw_xywh_rectangle_filled(280,300, 20,75,arcade.csscolor.SIENNA)
 arcade.draw_arc_filled(280, 350, 20, 100, start_angle=100,end_angle=180,tilt_angle=450, color=arcade.csscolor.LIGHT_GREEN)
 arcade.draw_arc_filled(290, 350, 20, 100, start_angle=100,end_angle=180,tilt_angle=-130, color=arcade.csscolor.LIGHT_GREEN)
+#Pine Tree Code
 arcade.draw_xywh_rectangle_filled(420, 300, 20, 71, arcade.csscolor.SIENNA)
 arcade.draw_triangle_filled(390, 320, 470, 320, 430, 340 ,arcade.csscolor.LIGHT_GREEN)
 arcade.draw_triangle_filled(395, 330, 465, 330, 430, 355 ,arcade.csscolor.LIGHT_GREEN)
@@ -24,7 +32,22 @@ arcade.draw_triangle_filled(400, 340, 460, 340, 430, 370 ,arcade.csscolor.LIGHT_
 arcade.draw_triangle_filled(405, 340, 455, 340, 430, 385 ,arcade.csscolor.LIGHT_GREEN)
 arcade.draw_triangle_filled(410, 340, 450, 340, 430, 400 ,arcade.csscolor.LIGHT_GREEN)
 
+#Sun Drawing
+arcade.draw_circle_filled(500,550,40, arcade.csscolor.YELLOW)
+arcade.draw_line(500, 550, 400, 550, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 600, 550, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 500, 450, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 500, 650, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 550, 600, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 550, 500, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 450, 600, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 450, 500, arcade.color.YELLOW, 3)
+
+#Text Generation
+arcade.draw_text("The Pine Tree Took Forever to Code!", 75, 230, arcade.color.FLORAL_WHITE, 20)
+
 arcade.finish_render()
+#Essentially Finishes the Image
+
 arcade.run()
 # This code makes the window stay open
-#make sure to add more comments
