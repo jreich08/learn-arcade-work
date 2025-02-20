f21e4df9d84792e976ce37ca0b27588d37d222a7
jreich08 <reichelj@hartwick.edu>
2025-02-20 10:00:02 -0500

    This is a short example of how variables get defined.

---
+++ Testing/Drawing with functions in class.py
@@ -0,0 +1,54 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+def draw_grass():
+    # Draw the ground
+    arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 3, 0, arcade.color.AIR_SUPERIORITY_BLUE)
+
+def draw_snow_person(x, y):
+    # Draw a point at x,y for reference
+    #arcade.draw_point(x, y, arcade.color.RED, 5)
+
+    # Snow
+    arcade.draw_circle_filled(x, 60 + y, 60, arcade.color.WHITE)
+    arcade.draw_circle_filled(x, 140 + y, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(x, 200 + y, 40, arcade.color.WHITE)
+
+    # Eyes
+    arcade.draw_circle_filled(x - 15, 210 + y, 5, arcade.color.BLACK)
+    arcade.draw_circle_filled(15 + x, 210 + y, 5, arcade.color.BLACK)
+#Draw Everything
+def on_draw(delta_time):
+
+    arcade.start_render()
+
+    draw_grass()
+
+    # Draw a snow person
+    draw_snow_person(on_draw.snow_person1_x, 140)
+    draw_snow_person(on_draw.snow_person1_y, 120)
+    draw_snow_person(600, 120)
+
+    #Add One to the X Value, making the snow person move right
+    #Negative numbers move left. Larger numbers move faster
+    on_draw.snow_person1_x +=1
+    on_draw.snow_person1_y +=-1
+
+# Create a value that our on_draw.snow_person1_x will start at
+on_draw.snow_person1_x = 150
+on_draw.snow_person1_y = 600
+def main():
+    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
+    arcade.set_background_color(arcade.color.DARK_BLUE)
+
+
+    #  Finish and run
+    #arcade.finish_render()
+    # Call on draw every 60th of a second
+    arcade.schedule(on_draw, 1/60)
+    arcade.run()
+
+# Call The Main Function to Get the Program Started
+main()
