4842b667d2e0c5e148875d7256dfe4fd61eb8477
jreich08 <reichelj@hartwick.edu>
2025-02-28 09:08:52 -0500

    This is short code showing how to draw with functions.

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -1,4 +1,13 @@
+from pycparser.c_ast import Break
+
+
 def main():
+    miles_natives_traveled = -20
+    miles_traveled = 0
+    drinks_in_canteen = 3
+    thirst_status = 0
+    camel_status = 0
+
     print("Welcome to Camel!")
     print("You have stolen a camel to make your way across the great Mobi desert.")
     print("The natives want their camel back and are chasing you down!")
@@ -20,6 +29,17 @@ def main():
     if user_choice.upper() == "Q":
             done =True
             print("OK, game over.")
+
+    if user_choice.upper() == "E":
+        print("My Status")
+
+    if user_choice.upper() == "A":
+            done  =True
+
+
+            print("You Reach for your canteen and take a long drink.")
+
+
 
 
 
