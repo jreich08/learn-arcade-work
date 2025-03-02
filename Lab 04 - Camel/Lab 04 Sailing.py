ec50abc286e7130a48fd531c8373cb57678dc176
jreich08 <reichelj@hartwick.edu>
2025-03-02 11:16:54 -0500

    This is short code showing how to draw with functions.

--- Lab 04 - Camel/Lab 04 Sailing.py
+++ Lab 04 - Camel/Lab 04 Sailing.py
@@ -2,34 +2,27 @@ import random
 
 from pycparser.c_ast import Break
 
-
-# This should provide a randomly generated travel distance
-
-#def get_travel_distance_capitan_half_sail(half_sail_mode):
-    #if half_sail_mode =="B": # Half Sail
-        #return random.randint(90,100)
-
-#def get_travel_distance_capitan_full_sail(full_sail_mode):
-    #if full_sail_mode == "C": #Full Sail
-        #return random.randint(180,300)
-
 def main():
     # Variable Set
     miles_pirates_sailed = -20
     miles_sailed = 0
     rations_on_board=5
-    ship_status=100
+    crew_status= 100
+
     def half_sail_distance(half_sail_mode):
         if half_sail_mode == "B":  # Half Sail
             return random.randint(90, 100)
-
+        return 0
     def full_sail_distance(full_sail_mode):
         if full_sail_mode == "C":  # Full Sail
             return random.randint(180, 300)
+        return 0
 
     def distance_pirates_sail(sail_mode):
-        if sail_mode == "B" or sail_mode == "C":
+        if sail_mode == "B" or sail_mode == "C" or "D":
             return random.randint(21,300)
+        return 0
+
 
     # Introduction to the Game
     print("Welcome to Full Sail!")
@@ -53,12 +46,18 @@ def main():
         user_choice = input("What is your choice? ")
 
     # Defining What Each Response Means
+        # Serve the Crew Rations
         if user_choice.upper() == "A":
+            print("You order the chef to prepare rations for the crew. Crew morale improves.")
             rations_on_board  = rations_on_board - 1
-        print("Remaining Rations",rations_on_board)
+            print("Remaining Rations",rations_on_board)
 
+            crew_status = crew_status + random.randint(9, 24)
+            print("Crew Morale:", crew_status)
+
+        # Half Sail
         if user_choice.upper() == "B":
-            print("The ships bell rings! You call out,\"full sail ahead men!\"")
+            print("The ships bell rings! You call out,\"Half-sail ahead men!\"")
             print("The crew hoists the main halyard and you watch the sails unfurl and fill with wind.")
             print("The ship leaps forward and begins sailing quickly.")
             distance = half_sail_distance(user_choice)
@@ -68,17 +67,63 @@ def main():
             miles_pirates_sailed += pirate_distance
             print("Caution pirates follow! They sailed miles:",miles_pirates_sailed)
 
+            crew_status = crew_status - random.randint (9,24)
+            print("Crew Morale:", crew_status)
 
+       # Full Sail
+        if user_choice.upper() == "C":
+            print("The ships bell rings! You call out,\"full sail ahead men!\"")
+            print("The crew hoists the main halyard and you watch the sails unfurl and fill with wind.")
+            print("The ship leaps forward and begins sailing quickly.")
+            distance = full_sail_distance(user_choice)
+            miles_sailed += distance
+            print("You sailed miles:", miles_sailed)
+            pirate_distance = distance_pirates_sail(user_choice)
+            miles_pirates_sailed += pirate_distance
+            print("Caution pirates follow! They sailed miles:", miles_pirates_sailed)
+
+            crew_status = crew_status - random.randint(19, 49)
+            print("Crew Morale:", crew_status)
+
+        # Check Status
         if user_choice.upper() == "E":
             print("My Status")
-            print("Ship Integrity:",ship_status)
+            print("Crew Morale:",crew_status)
             print("Number of Rations:",rations_on_board)
             print("Number of Miles Sailed:",miles_sailed)
             print("Number of Miles Pirates Sailed:",miles_pirates_sailed)
 
+        if user_choice.upper() == "D":
+            print("You sail into a small harbor on a passing island. You use company funds to resupply the ship.")
+            rations_on_board = rations_on_board + 3
+            print("Remaining Rations", rations_on_board)
+            pirate_distance = distance_pirates_sail(user_choice)
+            miles_pirates_sailed += pirate_distance
+            print("Caution pirates follow! They sailed miles:", miles_pirates_sailed)
 
+        # Game Failure Status Player Quits
         elif user_choice.upper() == "Q":
             done=True
             print("You abandoned your voyage.")
+
+        # Game Failure Status Pirates
+        if miles_pirates_sailed >= miles_sailed:
+            print("You failed to outrun the pirates! They will now butcher your crew and steal your cargo.")
+            print("You lost the game, better luck next time!")
+            done=True
+       # Game Failure Status Morale
+        if crew_status <= 0:
+            print("You failed to keep your crew morale high enough.")
+            print("They grew sick and tired of your leadership and threw you overboard!")
+            print("You lost the game, better luck next time!")
+            done = True
+
+        # Game Victory Conditions
+        if miles_sailed >= 1445:
+            print("You made it to Bermuda!")
+            print("You and your crew are now safe from pirates!")
+            print("You can now begin outfitting your ship for long voyage back to Holland.")
+            print("Congratulations, you won the game! Good work capitan!")
+            done = True
 
 main()
