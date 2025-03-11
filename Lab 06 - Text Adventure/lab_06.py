0fd1b1cadac1e803229be1bfc4a66a94b866ac17
jreich08 <reichelj@hartwick.edu>
2025-03-11 10:00:13 -0400

    This is the loopy lab drawing many shapes

---
+++ Lab 06 - Text Adventure/lab_06.py
@@ -0,0 +1,29 @@
+class Room:
+    def __init__(self):
+        self.Description=description
+        self.North= north
+        self.East= east
+        self.South= south
+        self.West= west
+
+room0 = Room("You are standing on Longevity Hill. It is a well lit night from the full moon")
+
+
+
+
+
+
+def main():
+
+    print ("The year is 1908 under the Qing Dynasty, you serve as a loyal eunuch under Emperor Guangxu.")
+    print ("Our once mighty and feared Empire stands on the brink of collapse.")
+    print("Your revered Emperor Guangxu has been held under house arrest by Dowager Cixi.")
+    print("Something is afoot, you suspect conspiracy against your Emperor")
+    print("You have your suspicions but must investigate further.")
+    print("Tonight you tend to your duties at the summer Yingtai Palace, on Longevity Hill' where you overhear a woman's voice?")
+    print("Talking softly she discussed her sinister plans to ensure the Emperor falls ill.")
+    print("Time is short, you must find the conspirator and put and end to the plot and save the Qing Dynasty.")
+
+
+
+main()
