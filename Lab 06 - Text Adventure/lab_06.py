782c2a617c57d6bee980156036226ba73e59503d
jreich08 <reichelj@hartwick.edu>
2025-03-12 08:15:31 -0400

    This is the loopy lab drawing many shapes

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,5 +1,5 @@
 class Room:
-    def __init__(self, description,north=0,east=0,south=0 ,west=0):
+    def __init__(self, description, north, east, south, west):
         self.Description=description
         self.North= north
         self.East= east
@@ -10,62 +10,84 @@ class Room:
 def main():
     done=False
     current_room = 0
-
-    room0 = Room("You are standing on Longevity Hill. It is a well lit night from the full moon",
+    room_list = []
+    #0
+    room = Room("You are standing on Longevity Hill. It is a well lit night from the full moon",
                  north=1,
                  east=5,
                  south=None,
                  west=3
                  )
-    room1 = Room("You have walked onto the Seventeen Arch bridge. Lit by newly installed electric light.",
+    room_list.append(room)
+    #1
+    room = Room("You have walked onto the Seventeen Arch bridge. Lit by newly installed electric light.",
                  north=2,
                  east=None,
                  south=0,
                  west=None
                  )
-    room2 = Room("You entered the Hall of Benevolence and Longevity it's stunning facades dimly lit by moonlight.",
+    room_list.append(room)
+    #2
+    room = Room("You entered the Hall of Benevolence and Longevity it's stunning facades dimly lit by moonlight.",
                  north=None,
                  east=4,
                  south=1,
                  west=6,
                  )
-    room3 = Room("You enter the holy Tower of Buddhist Incense, your nose fills with the thick smoke of incense.",
+    room_list.append(room)
+    #3
+    room = Room("You enter the holy Tower of Buddhist Incense, your nose fills with the thick smoke of incense.",
                  north=None,
                  east=0,
                  south=None,
                  west=None,
                  )
-    room4 = Room("You walk upon a dock until you reach a small row boat, you paddle out to the tranquil Marble Boat",
+    room_list.append(room)
+    #4
+    room = Room("You walk upon a dock until you reach a small row boat, you paddle out to the tranquil Marble Boat",
                  north=None,
                  east=None,
                  south=None,
                  west=2
                  )
-    room5 = Room(
+    room_list.append(room)
+    #5
+    room = Room(
         "You walk into the Garden of Harmonious Pleasures. This garden is the pinnacle of beauty and one of your favorite places in the palace.",
         north=None,
         east=None,
         south=None,
         west=5
         )
-    room6 = Room("You enter the rear Pavilion of the hall of Serenity.",
+    room_list.append(room)
+    #6
+    room = Room("You enter the rear Pavilion of the hall of Serenity.",
                  north=None,
                  east=2,
                  south=None,
                  west=None
                  )
+    room_list.append(room)
 
-    room_list = [room0, room1, room2, room3, room4, room5, room6]
-    print(room0)
+
+    print(room)
 
     print ("The year is 1908 under the Qing Dynasty, you serve as a loyal eunuch under Emperor Guangxu.")
+    print()
     print ("Our once mighty and feared Empire stands on the brink of collapse.")
+    print()
     print("Your revered Emperor Guangxu has been held under house arrest by Dowager Cixi.")
+    print()
     print("Something is afoot, you suspect conspiracy against your Emperor")
+    print()
     print("You have your suspicions but must investigate further.")
+    print()
     print("Tonight you tend to your duties at the summer Yingtai Palace, on Longevity Hill' where you overhear a woman's voice?")
+    print()
     print("Talking softly she discussed her sinister plans to ensure the Emperor falls ill.")
+    print()
     print("Time is short, you must find the conspirator and put and end to the plot and save the Qing Dynasty.")
+    print()
 
     while not done:
         print(room_list[current_room].Description)
@@ -75,12 +97,21 @@ def main():
 
 
         user_choice=input("Where would you like to go? (north, south, east, west, or quit)").lower()
-        if user_choice == "north" or "NORTH" or "N" or "n" or "North" and room_list[current_room].North is not None:
+        if user_choice.lower() == "north" or user_choice.lower() == "n" and room_list[current_room].North is not None:
             current_room = room_list[current_room].North
             print("You moved North")
-        elif user_choice == "east" and room_list[current_room].East is not None:
-            current_room = room_list[current_room].North
+        elif user_choice.lower() == "east" or user_choice.lower() == "e" and room_list[current_room].East is not None:
+            current_room = room_list[current_room].East
             print("You moved East")
+        elif user_choice.lower() == "west" or user_choice.lower() == "w" and room_list[current_room].West is not None:
+            current_room= room_list[current_room].East
+            print("You moved West")
+        elif user_choice.lower() == "south" or user_choice.lower() == "s" and room_list[current_room].West is not None:
+            current_room= room_list[current_room].South
+            print("You moved South")
+        elif user_choice.lower() == "quit" or user_choice.lower() == "q":
+            print("You quit the game goodbye!")
+            done=True
 
     print()
 
