f41e2820f2980f58ed61efa5aef70b1c401ebb1a
jreich08 <reichelj@hartwick.edu>
2025-03-24 08:56:03 -0400

    This is my room based text adventure game

---
+++ Testing/Test_23_03-2025.py
@@ -0,0 +1,15 @@
+class Dog:
+    def __init__(self):
+        self.age=0
+        self.name=""
+        self.weight=0
+
+    def bark(self):
+        print(self.name + " says \"Woof\"!")
+
+#Instantisating the Dog object      
+my_dog = Dog ()
+
+my_dog.name = "Spot"
+my_dog.weight = 20
+my_dog.age = 3
