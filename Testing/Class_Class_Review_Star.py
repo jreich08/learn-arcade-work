061363448e27130062f17fec299606f4acfb1824
jreich08 <reichelj@hartwick.edu>
2025-03-25 10:02:25 -0400

    This is my room based text adventure game

---
+++ Testing/Class_Class_Review_Star.py
@@ -0,0 +1,10 @@
+class Star:
+    def __init__(self):
+        self.name = ""
+    def star_born(self):
+        print("A star is born!")
+def main():
+    my_star = Star()
+    my_star.star_born()
+
+main()
