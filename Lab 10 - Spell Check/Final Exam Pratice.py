2934da3c247ba51e93b3cc78160faede0a870dec
jreich08 <reichelj@hartwick.edu>
2025-05-08 09:55:42 -0400

    Pizza Dash Game for Final

---
+++ Lab 10 - Spell Check/Final Exam Pratice.py
@@ -0,0 +1,38 @@
+
+"""_____________PRINT LAST AND FIRST NAME________________"""
+#user_name = input("What is your first and last name?") or user_name.lower() == "john"
+
+#def main():
+#    print(user_name)
+
+#main()
+"""____________PRINT LAST AND FIRST NAME CODE___________"""
+
+
+"""___________DRAW A CHICKEN____________________________"""
+#print(" /\"\"\\            '  ")
+#print("<>^  L__________/ |")
+#print(" `) /`     '     /")
+#print("  \\ ` ___'      /")
+#print("   ` \'\" ; \\ )`")
+#print("         _/_Y")
+"""___________DRAW A CHICKEN____________________________"""
+
+
+"""___________PRINT NUMBERS 100 TO 200_________________ """
+def number_print():
+    i = 200
+    while i <= 200 or i >= 100:
+        print(i)
+        i -= 1
+
+#THIS IS BROKEN!!!
+
+
+
+
+number_print()
+
+
+
+
